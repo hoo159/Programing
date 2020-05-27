ae3d96a37b7514d142160b0c824ad493271e231e
hoo159 <65270295+hoo159@users.noreply.github.com>
2020-05-27 15:51:56 +0900

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+#산술 연산은 두 수를 연산해서 수를 내어주는 연산
+a=int(input("첫 번째 정수 : "))
+b=int(input("두 번째 정수 : "))
+
+print(a,"**",b,"=",(a**b))
+print(a,"+",b,"=",(a+b))
+print(a,"-",b,"=",(a-b))
+print(a,"*",b,"=",(a*b))
+print(a,"/",b,"=",(a/b))
+print(a,"%",b,"=",(a%b))
+print(a,"//",b,"=",(ab))
---
+++ logic.py
@@ -0,0 +1,8 @@
+#논리 연산은 논리값(boolean)연산해서 수를 내어주는 연산
+
+a=input("첫 번째 논리값(True or False) : ")
+b=input("두 번째 논리값(True or False) : ")
+
+print("not", a, "==>", (not a))
+print(a, "and", b, "==>", (a and b))
+print(a, "or", b, "==>", (a or b))
